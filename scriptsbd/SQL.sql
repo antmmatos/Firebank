9f264c8c7191dbb187f936339225e00de4aececb
SrD4rkOficial <antonio.martim.matos@gmail.com>
2022-06-09 17:07:08 +0100

    Commit

--- scriptsbd/SQL.sql
+++ scriptsbd/SQL.sql
@@ -15,17 +15,15 @@
 
 
 -- A despejar estrutura da base de dados para PSI20L_AntonioMatos_2220077
-DROP DATABASE IF EXISTS "PSI20L_AntonioMatos_2220077";
 CREATE DATABASE IF NOT EXISTS "PSI20L_AntonioMatos_2220077";
 USE "PSI20L_AntonioMatos_2220077";
 
 -- A despejar estrutura para tabela PSI20L_AntonioMatos_2220077.Accounts
-DROP TABLE IF EXISTS "Accounts";
 CREATE TABLE IF NOT EXISTS "Accounts" (
 	"ID" INT NOT NULL,
 	"Account_Owner" INT NOT NULL,
 	"IBan" VARCHAR(50) NOT NULL COLLATE 'Latin1_General_CI_AI',
-	"Balance" INT NOT NULL DEFAULT '(0)',
+	"Balance" DECIMAL(15,2) NOT NULL DEFAULT '(0)',
 	"AccountName" VARCHAR(30) NULL DEFAULT NULL COLLATE 'Latin1_General_CI_AI',
 	PRIMARY KEY ("ID"),
 	FOREIGN KEY INDEX "FK__Accounts__Accoun__30F848ED" ("Account_Owner"),
@@ -35,10 +33,11 @@ CREATE TABLE IF NOT EXISTS "Accounts" (
 -- A despejar dados para tabela PSI20L_AntonioMatos_2220077.Accounts: -1 rows
 DELETE FROM "Accounts";
 /*!40000 ALTER TABLE "Accounts" DISABLE KEYS */;
+INSERT INTO "Accounts" ("ID", "Account_Owner", "IBan", "Balance", "AccountName") VALUES
+	(6, 6, 'PT50 073848931637867529438', 0, 'Cavalo');
 /*!40000 ALTER TABLE "Accounts" ENABLE KEYS */;
 
 -- A despejar estrutura para tabela PSI20L_AntonioMatos_2220077.Cards
-DROP TABLE IF EXISTS "Cards";
 CREATE TABLE IF NOT EXISTS "Cards" (
 	"ID" INT NOT NULL,
 	"CardNumber" VARCHAR(16) NOT NULL COLLATE 'Latin1_General_CI_AI',
@@ -49,18 +48,19 @@ CREATE TABLE IF NOT EXISTS "Cards" (
 	"isFreeze" BIT NOT NULL DEFAULT '(0)',
 	"isActivating" BIT NOT NULL DEFAULT '(0)',
 	"ActivationCode" VARCHAR(8) NULL DEFAULT NULL COLLATE 'Latin1_General_CI_AI',
-	PRIMARY KEY ("ID"),
 	FOREIGN KEY INDEX "FK__Cards__AccountID__34C8D9D1" ("AccountID"),
+	PRIMARY KEY ("ID"),
 	CONSTRAINT "FK__Cards__AccountID__34C8D9D1" FOREIGN KEY ("AccountID") REFERENCES "Accounts" ("ID") ON UPDATE NO_ACTION ON DELETE NO_ACTION
 );
 
 -- A despejar dados para tabela PSI20L_AntonioMatos_2220077.Cards: -1 rows
 DELETE FROM "Cards";
 /*!40000 ALTER TABLE "Cards" DISABLE KEYS */;
+INSERT INTO "Cards" ("ID", "CardNumber", "CardExpireDate", "CardCVV", "AccountID", "isActivated", "isFreeze", "isActivating", "ActivationCode") VALUES
+	(15, '4115572586089005', '2024-01-21', '153', 6, b'1', b'0', b'1', '7VVKLPVH');
 /*!40000 ALTER TABLE "Cards" ENABLE KEYS */;
 
 -- A despejar estrutura para tabela PSI20L_AntonioMatos_2220077.Transactions
-DROP TABLE IF EXISTS "Transactions";
 CREATE TABLE IF NOT EXISTS "Transactions" (
 	"ID" INT NOT NULL,
 	"Sender_ID" INT NOT NULL,
@@ -70,24 +70,21 @@ CREATE TABLE IF NOT EXISTS "Transactions" (
 	"Quantity" INT NOT NULL,
 	"Date" DATE NOT NULL DEFAULT 'getdate()',
 	"Description" VARCHAR(50) NULL DEFAULT NULL COLLATE 'Latin1_General_CI_AI',
-	PRIMARY KEY ("ID"),
-	FOREIGN KEY INDEX "FK__Transacti__Sende__412EB0B6" ("Sender_Account"),
 	FOREIGN KEY INDEX "FK__Transacti__Recei__4316F928" ("Receiver_Account"),
-	FOREIGN KEY INDEX "FK__Transacti__Sende__403A8C7D" ("Sender_ID"),
+	PRIMARY KEY ("ID"),
 	FOREIGN KEY INDEX "FK__Transacti__Recei__4222D4EF" ("Receiver_ID"),
-	CONSTRAINT "FK__Transacti__Sende__412EB0B6" FOREIGN KEY ("Sender_Account") REFERENCES "Accounts" ("ID") ON UPDATE NO_ACTION ON DELETE NO_ACTION,
 	CONSTRAINT "FK__Transacti__Recei__4316F928" FOREIGN KEY ("Receiver_Account") REFERENCES "Accounts" ("ID") ON UPDATE NO_ACTION ON DELETE NO_ACTION,
-	CONSTRAINT "FK__Transacti__Sende__403A8C7D" FOREIGN KEY ("Sender_ID") REFERENCES "Users" ("ID") ON UPDATE NO_ACTION ON DELETE NO_ACTION,
 	CONSTRAINT "FK__Transacti__Recei__4222D4EF" FOREIGN KEY ("Receiver_ID") REFERENCES "Users" ("ID") ON UPDATE NO_ACTION ON DELETE NO_ACTION
 );
 
 -- A despejar dados para tabela PSI20L_AntonioMatos_2220077.Transactions: -1 rows
 DELETE FROM "Transactions";
 /*!40000 ALTER TABLE "Transactions" DISABLE KEYS */;
+INSERT INTO "Transactions" ("ID", "Sender_ID", "Sender_Account", "Receiver_ID", "Receiver_Account", "Quantity", "Date", "Description") VALUES
+	(20, 3, 1, 6, 6, 1, '2022-06-09', '7VVKLPVH');
 /*!40000 ALTER TABLE "Transactions" ENABLE KEYS */;
 
 -- A despejar estrutura para tabela PSI20L_AntonioMatos_2220077.Users
-DROP TABLE IF EXISTS "Users";
 CREATE TABLE IF NOT EXISTS "Users" (
 	"ID" INT NOT NULL,
 	"Username" VARCHAR(100) NOT NULL COLLATE 'Latin1_General_CI_AI',
@@ -109,7 +106,8 @@ CREATE TABLE IF NOT EXISTS "Users" (
 DELETE FROM "Users";
 /*!40000 ALTER TABLE "Users" DISABLE KEYS */;
 INSERT INTO "Users" ("ID", "Username", "Password", "Email", "MobilePhoneNumber", "Birthday", "NIF", "CC", "VerifiedEmail", "VerifiedMobilePhone", "is2FAEnabled", "LastIP", "isAdmin") VALUES
-	(3, 'Administrator', '87cc98676f7702ccf6d93e238979b068122d1b7bd751a2920050f1a7fc5fd13e', 'antonio.martim.matos@gmail.com', '929190105', '2022-05-10', '12345678', '23456789', b'1', b'1', b'0', '93.108.239.154', b'1');
+	(6, 'Administrator', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 'antonio.martim.matos@gmail.com', '929190105', '1999-06-06', '234567890', '123456789', b'1', b'1', b'0', 'fghjk,', b'0'),
+	(10, 'Cavalo', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 'cavalo@gmail.com', '123456788', '2022-06-06', '123456788', '123456788', b'0', b'0', b'0', '95.92.174.123', b'0');
 /*!40000 ALTER TABLE "Users" ENABLE KEYS */;
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
